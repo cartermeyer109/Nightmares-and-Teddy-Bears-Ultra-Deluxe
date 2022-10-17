a5fe3f4551c78ba326ecd0edd2484df02578d7dd
cartermeyer109 <67216868+cartermeyer109@users.noreply.github.com>
2022-10-17 09:13:31 -0500

    Im just testing a thing

--- Sprint 1/Assets/gremlinAttacking.cs
+++ Sprint 1/Assets/gremlinAttacking.cs
@@ -8,6 +8,7 @@ public class gremlinAttacking : StateMachineBehaviour
     override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         animator.SetBool("attacked", false);
+        //hello
     }
 
     // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
