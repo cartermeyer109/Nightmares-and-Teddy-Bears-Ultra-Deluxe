d1418b0bacae5038972b9aa01d5efe437a348d9f
njstoll <stolln0818@my.uwstout.edu>
2022-11-12 20:22:00 -0600

    level 2!!!

--- Sprint 1/Assets/CameraFollowPlayer.cs
+++ Sprint 1/Assets/CameraFollowPlayer.cs
@@ -15,8 +15,56 @@ public class CameraFollowPlayer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
-        this.transform.position = new Vector3(player.transform.position.x, 0, -10);
+        if (player.transform.position.x >= 0 && player.transform.position.x < 156) //player is to the right of start area and to the left of end area
+        {
+            if (player.transform.position.y >= 0)
+            {
+                if (player.transform.position.y < 4)
+                {
+                    this.transform.position = new Vector3(player.transform.position.x, ((player.transform.position.y)), -10);
+                }
+                else
+                {
+                    this.transform.position = new Vector3(player.transform.position.x, 4, -10);
+                }
+            }
+            else
+            {
+                this.transform.position = new Vector3(player.transform.position.x, 0, -10);
+            }
+        }
+        else if (player.transform.position.x < 0)
+        {
+            if (player.transform.position.y >= 0) // above the ground level
+            {
+                if (player.transform.position.y < 4) // celing cap
+                {
+                    this.transform.position = new Vector3(0, ((player.transform.position.y)), -10);
+                }
+                else //somewhere between the ground level and the celing cap
+                {
+                    this.transform.position = new Vector3(0, 4, -10);
+                }
+            }
+            else
+                this.transform.position = new Vector3(0, 0, -10);
+        }
+        else if (player.transform.position.x >= 156)
+        {
+            if (player.transform.position.y >= 0) // above the ground level
+            {
+                if (player.transform.position.y < 4) // celing cap
+                {
+                    this.transform.position = new Vector3(156, ((player.transform.position.y)), -10);
+                }
+                else //somewhere between the ground level and the celing cap
+                {
+                    this.transform.position = new Vector3(156, 4, -10);
+                }
+            }
+            else
+                this.transform.position = new Vector3(156, 0, -10);
+        }
     }
 }
 
