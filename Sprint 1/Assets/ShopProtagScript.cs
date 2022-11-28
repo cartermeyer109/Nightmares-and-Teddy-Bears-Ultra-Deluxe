60587c9b0a042ccf378f402adec13c815ff49eaf
njstoll <stolln0818@my.uwstout.edu>
2022-11-28 10:59:49 -0600

    Added scene transition (in shop press space)

---
+++ Sprint 1/Assets/ShopProtagScript.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class ShopProtagScript : MonoBehaviour
+{
+    GameObject trackerObject;
+    levelTracker LevelTracker;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        trackerObject = GameObject.Find("LevelTracker");
+        LevelTracker = trackerObject.gameObject.GetComponent<levelTracker>();
+        LevelTracker.nextLevel();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space)){
+            //Debug.Log("test");
+            SceneManager.LoadScene(LevelTracker.getLevel());
+            //SceneManager.LoadScene("Tutorial Level");
+        }
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            //Debuging only
+            LevelTracker.nextLevel();
+            Debug.Log("Next Level");
+        }
+    }
+}
